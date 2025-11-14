91b66a3d062d1018edb78b2982057985a2b5ff91
Mark Sobkow <mark.sobkow@gmail.com>
2025-11-13 19:37:13 -0600

    2025-11-13 19:35 Relicense the manufactured code under Dual GPLv3/Commercial

--- src/main/java/org/msscf/msscf/v2_13/cfcore/CFGenKb/ICFGenKbGelExecutableFactory.java
+++ src/main/java/org/msscf/msscf/v2_13/cfcore/CFGenKb/ICFGenKbGelExecutableFactory.java
@@ -4,35 +4,26 @@
 /*
  *	org.msscf.msscf.CFCore
  *
- *	Copyright (c) 2020 Mark Stephen Sobkow
+ *	Copyright (c) 2016-2025 Mark Stephen Sobkow
  *	
  *	MSS Code Factory CFCore 2.13 Generation Knowledgebase
  *	
- *	Copyright 2020 Mark Stephen Sobkow
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
  *	
- *		This file is part of MSS Code Factory.
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
  *	
- *		MSS Code Factory is available under dual commercial license from Mark Stephen
- *		Sobkow, or under the terms of the GNU Lesser General Public License, Version 3
- *		or later.
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *	
- *	    MSS Code Factory is free software: you can redistribute it and/or modify
- *	    it under the terms of the GNU Lesser General Public License as published by
- *	    the Free Software Foundation, either version 3 of the License, or
- *	    (at your option) any later version.
- *	
- *	    MSS Code Factory is distributed in the hope that it will be useful,
- *	    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	    GNU Lesser General Public License for more details.
- *	
- *	    You should have received a copy of the GNU Lesser General Public License
- *	    along with MSS Code Factory.  If not, see <https://www.gnu.org/licenses/>.
- *	
- *	Donations to support MSS Code Factory can be made at
- *	https://www.paypal.com/paypalme2/MarkSobkow
- *	
- *	Please contact Mark Stephen Sobkow at mark.sobkow@gmail.com for commercial licensing.
+ *	If you wish to modify and use this code without publishing your changes,
+ *	or integrate it with proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
  *
  *	Manufactured by MSS Code Factory 2.12
  */
--- src/main/java/org/msscf/msscf/v2_13/cfcore/CFGenKbObj/CFGenKbGelSwitchObj.java
+++ src/main/java/org/msscf/msscf/v2_13/cfcore/CFGenKbObj/CFGenKbGelSwitchObj.java
@@ -3,35 +3,26 @@
 /*
  *	org.msscf.msscf.CFCore
  *
- *	Copyright (c) 2020 Mark Stephen Sobkow
+ *	Copyright (c) 2016-2025 Mark Stephen Sobkow
  *	
  *	MSS Code Factory CFCore 2.13 Generation Knowledgebase
  *	
- *	Copyright 2020 Mark Stephen Sobkow
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
  *	
- *		This file is part of MSS Code Factory.
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
  *	
- *		MSS Code Factory is available under dual commercial license from Mark Stephen
- *		Sobkow, or under the terms of the GNU Lesser General Public License, Version 3
- *		or later.
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *	
- *	    MSS Code Factory is free software: you can redistribute it and/or modify
- *	    it under the terms of the GNU Lesser General Public License as published by
- *	    the Free Software Foundation, either version 3 of the License, or
- *	    (at your option) any later version.
- *	
- *	    MSS Code Factory is distributed in the hope that it will be useful,
- *	    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	    GNU Lesser General Public License for more details.
- *	
- *	    You should have received a copy of the GNU Lesser General Public License
- *	    along with MSS Code Factory.  If not, see <https://www.gnu.org/licenses/>.
- *	
- *	Donations to support MSS Code Factory can be made at
- *	https://www.paypal.com/paypalme2/MarkSobkow
- *	
- *	Please contact Mark Stephen Sobkow at mark.sobkow@gmail.com for commercial licensing.
+ *	If you wish to modify and use this code without publishing your changes,
+ *	or integrate it with proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
  *
  *	Manufactured by MSS Code Factory 2.12
  */
@@ -292,10 +283,6 @@ public class CFGenKbGelSwitchObj
 			throw new RuntimeException( S_ProcName + "Could not resolve Value expansion \"" + valueExpansion + "\"" );
 		}
 
-		if (MssCFEngine.getDebugMode()) {
-			System.err.println("Expanding " + valueItem.getRequiredName());
-		}
-
 		String valueStrValue;
 		if( valueItem instanceof MssCFGenFileObj ) {
 			valueStrValue = ((MssCFGenFileObj)valueItem).expandBody( genContext );
@@ -337,9 +324,6 @@ public class CFGenKbGelSwitchObj
 			if( genItem == null ) {
 				throw new RuntimeException( S_ProcName + "Could not resolve expansion \"" + effectiveExpansion + "\"" );
 			}
-			if (MssCFEngine.getDebugMode()) {
-				System.err.println("Expanding " + genItem.getRequiredName());
-			}
 
 			if( genItem instanceof MssCFGenFileObj ) {
 				retval = ((MssCFGenFileObj)genItem).expandBody( genContext );
@@ -376,9 +360,6 @@ public class CFGenKbGelSwitchObj
 			ICFGenKbGenItemObj genItem = genContext.getGenEngine().findContextItem(genContext, effectiveExpansion );
 			if( genItem == null ) {
 				throw new RuntimeException( S_ProcName + "Could not resolve expansion \"" + effectiveExpansion + "\"" );
-			}
-			if (MssCFEngine.getDebugMode()) {
-				System.err.println("Expanding " + genItem.getRequiredName());
 			}
 
 			if( genItem instanceof MssCFGenFileObj ) {
@@ -423,9 +404,7 @@ public class CFGenKbGelSwitchObj
 			if( genItem == null ) {
 				throw new RuntimeException( S_ProcName + "Could not resolve expansion \"" + effectiveExpansion + "\"" );
 			}
-			if (MssCFEngine.getDebugMode()) {
-				System.err.println("Expanding " + genItem.getRequiredName());
-			}
+
 			if( genItem instanceof MssCFGenFileObj ) {
 				retval = ((MssCFGenFileObj)genItem).expandBody( genContext );
 			}
